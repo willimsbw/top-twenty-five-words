48b0c761e56b7ad30d00af60a40c4e8fa81870ae
Bryan Williams <Bryan.williams@achiever.com>
2018-04-16 15:09:08 -0400

    src files into a single class

--- src/TopTwentyFiveWords.java
+++ src/TopTwentyFiveWords.java
@@ -1,4 +1,5 @@
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -23,14 +24,42 @@ import org.jsoup.Jsoup;
 public class TopTwentyFiveWords {
 	
 	/**
+	 * Creates or connects to specified database
+	 * 
+	 * @param databaseName Name of the database
+	 * @param user Username for mySQL
+	 * @param password Password for mySQL
+	 * @return Connection to database specified by databaseName 
+	 * @return null if exception thrown
+	 */
+	public static Connection connectTo(String databaseName, String user, String password) {
+		// url to connec to no mySQL but no specific database
+		String dbUrl = "jdbc:mysql://localhost:3306/?useSSL=false";
+		// sql statement to create new database if it doesn't already exist
+		String sql = "CREATE DATABASE IF NOT EXISTS " + databaseName;
+		try {
+			// register the jdbc driver
+			Class.forName("com.mysql.jdbc.Driver");
+			// connect to database
+			Connection con = DriverManager.getConnection(dbUrl, user, password);
+			// create database if it doesn't already exist
+			con.createStatement().executeUpdate(sql);
+			// update dbUrl
+			dbUrl = "jdbc:mysql://localhost:3306/" + databaseName + "?useSSL=false";
+			// return connect to specified database now that we know it for-sure exists
+			return DriverManager.getConnection(dbUrl, user, password);
+		} catch(Exception ex) {
+			System.out.println("Error connecting to " + dbUrl + ": " + ex);
+			return null;
+		}
+	}
+	
+	/**
 	 * SOME OF THIS COULD BE BROKEN OUT INTO METHODS. DO THIS ONCE I'VE WORKED THROUGH ALL FUNCTIONALITY.
 	 * 
 	 * @param args Don't do anything with passed-in args
 	 */
-	public static void main(String[] args) {
-		//create DbConnect object con
-		DbConnect con = new DbConnect();
-		
+	public static void main(String[] args) {		
 		// initialize userInput so we can accept information from the user
 		Scanner userInput = new Scanner(System.in);
 		
@@ -60,7 +89,7 @@ public class TopTwentyFiveWords {
 		String urlWordsEscaped = urlWords.replaceAll("'", "\\\\'");
 		
 		// create connection to database of user's choice called userDb. Will be created if it doesn't exist.
-		Connection userDb = con.connectTo(database, user, pass);
+		Connection userDb = connectTo(database, user, pass);
 		
 		// SQL statements to create the two tables we need (if they don't already exist) in the input database and pass in the url and the page's words
 		String useDatabase = "USE " + database + ";";
@@ -150,6 +179,6 @@ public class TopTwentyFiveWords {
 		} catch (SQLException e) {
 			System.out.print("Encountered error while closing connection to " + database);
 		}
-	}
+	}	
 
 }
